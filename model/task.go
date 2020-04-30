0aa89c9224b02408d82abff19580f922a3699fc4
dionomusuko <ruby.school114@gmail.com>
2020-04-30 22:32:28 +0900

    fix model

--- model/task.go
+++ model/task.go
@@ -1,9 +1,13 @@
 package model
 
+import "time"
+
 type Task struct {
-	ID        int    `gorm:"primary_key" json:"id"`
-	Time      int    `json:"time"`
-	Rep       int    `json:"rep"`
-	Set_Count int    `json:"set_count"`
-	Comment   string `json:"comment"`
+	ID        int       `gorm:"primary_key" json:"id"`
+	Time      int       `json:"time"`
+	Rep       int       `json:"rep"`
+	Set_Count int       `json:"set_count"`
+	Comment   string    `json:"comment"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
