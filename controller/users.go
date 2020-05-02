422f08fccbcc42e4b5caf9f7035d8b05c045c236
dionomusuko <ruby.school114@gmail.com>
2020-05-02 17:27:21 +0900

    add show

--- controller/users.go
+++ controller/users.go
@@ -4,6 +4,7 @@ import (
 	"github.com/dionomusuko/muscle2/db"
 	"github.com/dionomusuko/muscle2/model"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func GetUsers(c *gin.Context) {
@@ -14,4 +15,12 @@ func GetUsers(c *gin.Context) {
 }
 
 func ShowUser(c *gin.Context) {
+	id := c.Param("id")
+	var user model.User
+	db := db.NewDB()
+	if err := db.Where("id = ?", id).First(&user).Error; err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	c.JSON(http.StatusOK, &user)
 }
